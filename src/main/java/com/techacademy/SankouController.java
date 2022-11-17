bcd79aa50d22d6cc55a70373b5f8c1d46809dc97
Akira Kishimoto <akira_kishimoto@r.recruit.co.jp>
2022-11-17 21:58:07 +0900

    SankouEnzanshi

---
+++ src/main/java/com/techacademy/SankouController.java
@@ -0,0 +1,17 @@
+package com.techacademy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SankouController {
+    @RequestMapping("/sankou/{val1}/{val2}/")
+    public String sankou(@PathVariable int val1, @PathVariable int val2, Model model) {
+        model.addAttribute("val1", val1);
+        model.addAttribute("val2", val2);
+        return "sankou";
+    }
+
+}
